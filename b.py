33d9abf974ad58bef48b041b0e592d1425ce9309
Kurun <kurunpan09@gmail.com>
2021-08-08 22:53:02 +0900

    update template

--- b.py
+++ b.py
@@ -1,6 +1,8 @@
 import bisect,collections,copy,heapq,itertools,math,string,queue
 import sys
 
+sys.setrecursionlimit(300000)
+
 N, H = map(int, input().split())
 A = list(map(int, input().split()))
 A = [list(map(int, input().split())) for _ in range(H)]
